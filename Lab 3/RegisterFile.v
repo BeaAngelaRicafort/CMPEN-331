a798ff7d4ac187cce20d6aef38b7b8712ea48f4d
Bea Ricafort <144851872+BeaAngelaRicafort@users.noreply.github.com>
2025-12-24 19:19:42 -0500

    Update RegisterFile.v

--- Lab 3/RegisterFile.v
+++ Lab 3/RegisterFile.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/09/2025 04:24:10 PM
-// Design Name: 
-// Module Name: RegisterFile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module RegisterFile(
 
