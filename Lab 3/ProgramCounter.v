a5c2f41360b74de7a56b3d25e1b3a89418089e4c
Bea Ricafort <144851872+BeaAngelaRicafort@users.noreply.github.com>
2025-12-24 19:19:15 -0500

    Update ProgramCounter.v

--- Lab 3/ProgramCounter.v
+++ Lab 3/ProgramCounter.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/09/2025 10:12:44 AM
-// Design Name: 
-// Module Name: ProgramCounter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ProgramCounter(
 
