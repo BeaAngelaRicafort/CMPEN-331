32ebc3538f8f08d2c16285104e0b97a44c7e4065
Bea Ricafort <144851872+BeaAngelaRicafort@users.noreply.github.com>
2025-12-24 20:06:52 -0500

    Update WBMux.v

--- Lab 5/WBMux.v
+++ Lab 5/WBMux.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/22/2025 02:24:24 PM
-// Design Name: 
-// Module Name: WBMux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module WBMux(
 
