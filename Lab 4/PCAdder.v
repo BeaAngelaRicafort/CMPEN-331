b33655440ce540e147f004fa83af0c5020c512b7
Bea Ricafort <144851872+BeaAngelaRicafort@users.noreply.github.com>
2025-12-24 19:38:14 -0500

    Update PCAdder.v

--- Lab 4/PCAdder.v
+++ Lab 4/PCAdder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/09/2025 11:04:50 AM
-// Design Name: 
-// Module Name: PCAdder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module PCAdder(
 
