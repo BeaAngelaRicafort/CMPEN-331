808a6601353fecda08ec93bd84f3138976633fe8
Bea Ricafort <144851872+BeaAngelaRicafort@users.noreply.github.com>
2025-12-24 19:15:43 -0500

    Update IFID.v

--- Lab 3/IFID.v
+++ Lab 3/IFID.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/09/2025 11:10:09 AM
-// Design Name: 
-// Module Name: IFID
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module IFID(
 
